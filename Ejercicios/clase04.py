e95a01139dc606a4480352b34f5d396e28813561
HennerMJ <hennercalpe@hotmail.com>
2026-05-10 20:00:37 +0200

    Update clase04.py

--- Ejercicios/clase04.py
+++ Ejercicios/clase04.py
@@ -1,13 +1,14 @@
-# Crea un programa en Python que permita gestionar una lista de productos de una tienda.
-# El programa debe utilizar funciones para realizar las siguientes operaciones:
-# Agregar productos con su nombre y precio. 
-# Mostrar todos los productos disponibles. 
-# Buscar un producto por su nombre y mostrar su precio. 
-# Calcular el precio total de todos los productos.
-# Organiza tu código en funciones separadas para cada una de estas acciones.
-# El usuario debe poder elegir qué hacer mediante un menú simple.
-# Además, tendrás que utilizar estructuras de control como listas para poder almacenar todos los productos.
-
+""" 
+Crea un programa en Python que permita gestionar una lista de productos de una tienda.
+El programa debe utilizar funciones para realizar las siguientes operaciones:
+Agregar productos con su nombre y precio. 
+Mostrar todos los productos disponibles. 
+Buscar un producto por su nombre y mostrar su precio. 
+Calcular el precio total de todos los productos.
+Organiza tu código en funciones separadas para cada una de estas acciones.
+El usuario debe poder elegir qué hacer mediante un menú simple.
+Además, tendrás que utilizar estructuras de control como listas para poder almacenar todos los productos.
+"""
 productos = []
 
 def agregar_producto():
